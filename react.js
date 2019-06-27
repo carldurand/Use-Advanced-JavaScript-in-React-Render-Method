9a4e133448b6629d5aacf21d097081d816d9c939
Carl Durand <carldurand.student@careerdevs.com>
2019-06-27 15:59:43 +0000

    Pass Props to a Stateless Functional Component

--- react.js
+++ react.js
@@ -1,21 +1,26 @@
 
-class TypesOfFood extends React.Component {
+const CurrentDate = (props) => {
+  return (
+    <div>
+      { /* change code below this line */ }
+      <p>The current date is: {props.date}</p>
+      { /* change code above this line */ }
+    </div>
+  );
+};
+
+class Calendar extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
       <div>
-        <h1>Types of Food:</h1>
-        {/* change code below this line */}
-        <Fruits/>
-        <Vegetables/>
-        {/* change code above this line */}
+        <h3>What date is it?</h3>
+        { /* change code below this line */ }
+        <CurrentDate date={Date()}/>
+        { /* change code above this line */ }
       </div>
     );
   }
 };
-
-// change code below this line
-ReactDOM.render(<TypesOfFood/>, document.getElementById
-('challenge-node'))
