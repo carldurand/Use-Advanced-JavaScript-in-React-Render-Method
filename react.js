f180557063fb2985d8ef5c64c162aaf709021642
Carl Durand <carldurand.student@careerdevs.com>
2019-06-28 17:10:20 +0000

    Create a Controlled Input

--- react.js
+++ react.js
@@ -1,25 +1,24 @@
-
-const CurrentDate = (props) => {
-  return (
-    <div>
-      { /* change code below this line */ }
-      <p>The current date is: {props.date}</p>
-      { /* change code above this line */ }
-    </div>
-  );
-};
-
-class Calendar extends React.Component {
+class ControlledInput extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      input: ''
+    };
+    // change code below this line
+    this.handleChange = this.handleChange.bind(this);
+    // change code above this line
   }
+  // change code below this line
+    handleChange(event) {this.setState({input:event.target.value})}
+  // change code above this line
   render() {
     return (
       <div>
-        <h3>What date is it?</h3>
-        { /* change code below this line */ }
-        <CurrentDate date={Date()}/>
-        { /* change code above this line */ }
+        { /* change code below this line */}
+        <input type= "text" value= {this.state.input} onChange = {this.handleChange} />
+        { /* change code above this line */}
+        <h4>Controlled Input:</h4>
+        <p>{this.state.input}</p>
       </div>
     );
   }
