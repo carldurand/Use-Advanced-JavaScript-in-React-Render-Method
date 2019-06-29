30f6c4196c70e063fea8b0c7cb5d5efb192790d1
Carl Durand <carldurand.student@careerdevs.com>
2019-06-29 15:44:53 +0000

    Use Advanced JavaScript in React Render Method

--- react.js
+++ react.js
@@ -1,25 +1,72 @@
-class ControlledInput extends React.Component {
+const inputStyle = {
+  width: 235,
+  margin: 5
+}
+
+class MagicEightBall extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: ''
-    };
-    // change code below this line
+      userInput: '',
+      randomIndex: ''
+    }
+    this.ask = this.ask.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    // change code above this line
   }
-  // change code below this line
-    handleChange(event) {this.setState({input:event.target.value})}
-  // change code above this line
+  ask() {
+    if (this.state.userInput) {
+      this.setState({
+        randomIndex: Math.floor(Math.random() * 20),
+        userInput: ''
+      });
+    }
+  }
+  handleChange(event) {
+    this.setState({
+      userInput: event.target.value
+    });
+  }
   render() {
+    const possibleAnswers = [
+      'It is certain',
+      'It is decidedly so',
+      'Without a doubt', 
+      'Yes, definitely',
+      'You may rely on it',
+      'As I see it, yes',
+      'Outlook good',
+      'Yes',
+      'Signs point to yes',
+      'Reply hazy try again',
+      'Ask again later',
+      'Better not tell you now',
+      'Cannot predict now',
+      'Concentrate and ask again',
+      'Don\'t count on it', 
+      'My reply is no',
+      'My sources say no',
+      'Most likely',
+      'Outlook not so good',
+      'Very doubtful'
+    ];
+    const answer = possibleAnswers[this.state.randomIndex] // << change code here
     return (
       <div>
-        { /* change code below this line */}
-        <input type= "text" value= {this.state.input} onChange = {this.handleChange} />
-        { /* change code above this line */}
-        <h4>Controlled Input:</h4>
-        <p>{this.state.input}</p>
+        <input
+          type="text"
+          value={this.state.userInput}
+          onChange={this.handleChange}
+          style={inputStyle} /><br />
+        <button onClick={this.ask}>
+          Ask the Magic Eight Ball!
+        </button><br />
+        <h3>Answer:</h3>
+        <p>
+          { /* change code below this line */ }
+{answer}
+          { /* change code above this line */ }
+        </p>
       </div>
     );
   }
-}
+};
